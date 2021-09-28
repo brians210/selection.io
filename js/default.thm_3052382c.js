30015619b39fa2ecfc0687e7fbf646db45b4d2ae
brian.sham <brian.s@hdl2021.com>
2021-09-28 12:06:23 +0800

    selection version

--- js/default.thm_3052382c.js
+++ js/default.thm_3052382c.js
@@ -632,83 +632,793 @@ window.skins={};
 		return t;
 	};
 	return VSliderSkin;
-})(eui.Skin);generateEUI.paths['resource/skin/gotchaPageSkin.exml'] = window.skins.gotchaPageSkin = (function (_super) {
-	__extends(gotchaPageSkin, _super);
-	function gotchaPageSkin() {
+})(eui.Skin);generateEUI.paths['resource/skin/mainPageSkin.exml'] = window.skins.mainPageSkin = (function (_super) {
+	__extends(mainPageSkin, _super);
+	function mainPageSkin() {
 		_super.call(this);
-		this.skinParts = ["gotchaBtn"];
+		this.skinParts = ["enterBtn","importBtn","nameInput","msgLabel"];
 		
+		this.height = 720;
 		this.minHeight = 50;
 		this.minWidth = 100;
-		this.elementsContent = [this._Image1_i(),this.gotchaBtn_i()];
+		this.width = 1280;
+		this.elementsContent = [this._Rect1_i(),this.enterBtn_i(),this.importBtn_i(),this.nameInput_i(),this.msgLabel_i()];
 	}
-	var _proto = gotchaPageSkin.prototype;
+	var _proto = mainPageSkin.prototype;
 
-	_proto._Image1_i = function () {
-		var t = new eui.Image();
-		t.height = 1280;
-		t.source = "mainBg_jpg";
-		t.width = 720;
+	_proto._Rect1_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0x514242;
+		t.height = 720;
+		t.width = 1280;
 		return t;
 	};
-	_proto.gotchaBtn_i = function () {
+	_proto.enterBtn_i = function () {
 		var t = new eui.Button();
-		this.gotchaBtn = t;
-		t.label = "抽抽";
+		this.enterBtn = t;
+		t.label = "enter";
 		t.scaleX = 2;
 		t.scaleY = 2;
-		t.x = 289;
-		t.y = 1021;
+		t.x = 136;
+		t.y = 260;
 		return t;
 	};
-	return gotchaPageSkin;
+	_proto.importBtn_i = function () {
+		var t = new eui.Button();
+		this.importBtn = t;
+		t.label = "import";
+		t.scaleX = 2;
+		t.scaleY = 2;
+		t.visible = false;
+		t.x = 497;
+		t.y = 62;
+		return t;
+	};
+	_proto.nameInput_i = function () {
+		var t = new eui.TextInput();
+		this.nameInput = t;
+		t.height = 56;
+		t.width = 307;
+		t.x = 82.5;
+		t.y = 84;
+		return t;
+	};
+	_proto.msgLabel_i = function () {
+		var t = new eui.Label();
+		this.msgLabel = t;
+		t.text = "";
+		t.textColor = 0xe50d0d;
+		t.x = 87;
+		t.y = 178;
+		return t;
+	};
+	return mainPageSkin;
 })(eui.Skin);generateEUI.paths['resource/skin/resultPageSkin.exml'] = window.skins.resultPageSkin = (function (_super) {
 	__extends(resultPageSkin, _super);
 	function resultPageSkin() {
 		_super.call(this);
-		this.skinParts = ["backBtn","resultNumberLbl","resultTextLbl"];
+		this.skinParts = ["backBtn","previousBtn","nextBtn","pageNumberLbl","vLine","hLine","label","nameABLbl","assigneeABLbl","membershipABLbl","yrABLbl","starABLbl","stemABLbl","seasonABLbl","branchABLbl","constellationABLbl","actualABLbl","grpABInfo","nameALbl","assigneeALbl","membershipALbl","yrALbl","starALbl","stemALbl","seasonALbl","branchALbl","constellationALbl","actualALbl","grpA","nameBLbl","assigneeBLbl","membershipBLbl","yrBLbl","starBLbl","stemBLbl","seasonBLbl","branchBLbl","constellationBLbl","actualBLbl","grpB"];
 		
 		this.minHeight = 50;
 		this.minWidth = 100;
-		this.elementsContent = [this._Rect1_i(),this.backBtn_i(),this.resultNumberLbl_i(),this.resultTextLbl_i()];
+		this.elementsContent = [this._Rect1_i(),this.backBtn_i(),this.previousBtn_i(),this.nextBtn_i(),this.pageNumberLbl_i(),this._Group1_i()];
 	}
 	var _proto = resultPageSkin.prototype;
 
 	_proto._Rect1_i = function () {
 		var t = new eui.Rect();
 		t.fillColor = 0x000000;
-		t.height = 1280;
-		t.width = 720;
+		t.height = 720;
+		t.width = 1280;
 		return t;
 	};
 	_proto.backBtn_i = function () {
 		var t = new eui.Button();
 		this.backBtn = t;
 		t.label = "Back";
-		t.scaleX = 2;
-		t.scaleY = 2;
-		t.x = 248;
-		t.y = 1021;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.x = 1094;
+		t.y = 600;
 		return t;
 	};
-	_proto.resultNumberLbl_i = function () {
-		var t = new eui.Label();
-		this.resultNumberLbl = t;
-		t.text = "Label";
-		t.x = 345;
-		t.y = 138;
+	_proto.previousBtn_i = function () {
+		var t = new eui.Button();
+		this.previousBtn = t;
+		t.label = "previous";
+		t.x = 78;
+		t.y = 600;
 		return t;
 	};
-	_proto.resultTextLbl_i = function () {
+	_proto.nextBtn_i = function () {
+		var t = new eui.Button();
+		this.nextBtn = t;
+		t.label = "next";
+		t.x = 402;
+		t.y = 600;
+		return t;
+	};
+	_proto.pageNumberLbl_i = function () {
 		var t = new eui.Label();
-		this.resultTextLbl = t;
-		t.height = 155;
-		t.text = "Label";
+		this.pageNumberLbl = t;
+		t.text = "---";
+		t.x = 275;
+		t.y = 609;
+		return t;
+	};
+	_proto._Group1_i = function () {
+		var t = new eui.Group();
+		t.x = 83;
+		t.y = 41;
+		t.elementsContent = [this._Rect2_i(),this.vLine_i(),this.hLine_i(),this.label_i(),this.grpABInfo_i(),this.grpA_i(),this.grpB_i()];
+		return t;
+	};
+	_proto._Rect2_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0x5b4646;
+		t.height = 405;
+		t.width = 800;
+		return t;
+	};
+	_proto.vLine_i = function () {
+		var t = new eui.Group();
+		this.vLine = t;
+		t.elementsContent = [this._Rect3_i(),this._Rect4_i(),this._Rect5_i()];
+		return t;
+	};
+	_proto._Rect3_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xd81313;
+		t.height = 405;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 5;
+		t.x = 200;
+		t.y = 0;
+		return t;
+	};
+	_proto._Rect4_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 405;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 5;
+		t.x = 399.99999999999994;
+		t.y = 0;
+		return t;
+	};
+	_proto._Rect5_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 405;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 5;
+		t.x = 600;
+		return t;
+	};
+	_proto.hLine_i = function () {
+		var t = new eui.Group();
+		this.hLine = t;
+		t.elementsContent = [this._Rect6_i(),this._Rect7_i(),this._Rect8_i(),this._Rect9_i(),this._Rect10_i(),this._Rect11_i(),this._Rect12_i(),this._Rect13_i(),this._Rect14_i(),this._Rect15_i()];
+		return t;
+	};
+	_proto._Rect6_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 45;
+		return t;
+	};
+	_proto._Rect7_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 85;
+		return t;
+	};
+	_proto._Rect8_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 125;
+		return t;
+	};
+	_proto._Rect9_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 165;
+		return t;
+	};
+	_proto._Rect10_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 205;
+		return t;
+	};
+	_proto._Rect11_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 245;
+		return t;
+	};
+	_proto._Rect12_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 285;
+		return t;
+	};
+	_proto._Rect13_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 325;
+		return t;
+	};
+	_proto._Rect14_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 365;
+		return t;
+	};
+	_proto._Rect15_i = function () {
+		var t = new eui.Rect();
+		t.fillColor = 0xD81313;
+		t.height = 2;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.width = 800;
+		t.x = 0;
+		t.y = 405;
+		return t;
+	};
+	_proto.label_i = function () {
+		var t = new eui.Group();
+		this.label = t;
+		t.elementsContent = [this._Label1_i(),this._Label2_i(),this._Label3_i(),this._Label4_i(),this._Label5_i(),this._Label6_i(),this._Label7_i(),this._Label8_i(),this._Label9_i(),this._Label10_i()];
+		return t;
+	};
+	_proto._Label1_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "Name";
+		t.x = 10;
+		t.y = 10.000000000000007;
+		return t;
+	};
+	_proto._Label2_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "Assignee";
+		t.x = 10;
+		t.y = 50;
+		return t;
+	};
+	_proto._Label3_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "Membership";
+		t.x = 10;
+		t.y = 90;
+		return t;
+	};
+	_proto._Label4_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "年份";
+		t.x = 10;
+		t.y = 130;
+		return t;
+	};
+	_proto._Label5_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "星座";
+		t.x = 10;
+		t.y = 169.99999999999997;
+		return t;
+	};
+	_proto._Label6_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "天干";
+		t.x = 10;
+		t.y = 210;
+		return t;
+	};
+	_proto._Label7_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "四季";
+		t.x = 10;
+		t.y = 250;
+		return t;
+	};
+	_proto._Label8_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "⽇元地⽀";
+		t.x = 10;
+		t.y = 290;
+		return t;
+	};
+	_proto._Label9_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "星宿";
+		t.x = 10;
+		t.y = 330;
+		return t;
+	};
+	_proto._Label10_i = function () {
+		var t = new eui.Label();
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.text = "Actual %";
+		t.x = 10;
+		t.y = 370;
+		return t;
+	};
+	_proto.grpABInfo_i = function () {
+		var t = new eui.Group();
+		this.grpABInfo = t;
+		t.x = 205;
+		t.elementsContent = [this.nameABLbl_i(),this.assigneeABLbl_i(),this.membershipABLbl_i(),this.yrABLbl_i(),this.starABLbl_i(),this.stemABLbl_i(),this.seasonABLbl_i(),this.branchABLbl_i(),this.constellationABLbl_i(),this.actualABLbl_i()];
+		return t;
+	};
+	_proto.nameABLbl_i = function () {
+		var t = new eui.Label();
+		this.nameABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
 		t.textAlign = "center";
-		t.verticalAlign = "bottom";
-		t.width = 551;
-		t.x = 108;
-		t.y = 177;
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 10.000000000000007;
+		return t;
+	};
+	_proto.assigneeABLbl_i = function () {
+		var t = new eui.Label();
+		this.assigneeABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 54;
+		return t;
+	};
+	_proto.membershipABLbl_i = function () {
+		var t = new eui.Label();
+		this.membershipABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 94;
+		return t;
+	};
+	_proto.yrABLbl_i = function () {
+		var t = new eui.Label();
+		this.yrABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 134;
+		return t;
+	};
+	_proto.starABLbl_i = function () {
+		var t = new eui.Label();
+		this.starABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 174;
+		return t;
+	};
+	_proto.stemABLbl_i = function () {
+		var t = new eui.Label();
+		this.stemABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 214;
+		return t;
+	};
+	_proto.seasonABLbl_i = function () {
+		var t = new eui.Label();
+		this.seasonABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 254;
+		return t;
+	};
+	_proto.branchABLbl_i = function () {
+		var t = new eui.Label();
+		this.branchABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 294;
+		return t;
+	};
+	_proto.constellationABLbl_i = function () {
+		var t = new eui.Label();
+		this.constellationABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 334;
+		return t;
+	};
+	_proto.actualABLbl_i = function () {
+		var t = new eui.Label();
+		this.actualABLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 374;
+		return t;
+	};
+	_proto.grpA_i = function () {
+		var t = new eui.Group();
+		this.grpA = t;
+		t.x = 405;
+		t.elementsContent = [this.nameALbl_i(),this.assigneeALbl_i(),this.membershipALbl_i(),this.yrALbl_i(),this.starALbl_i(),this.stemALbl_i(),this.seasonALbl_i(),this.branchALbl_i(),this.constellationALbl_i(),this.actualALbl_i()];
+		return t;
+	};
+	_proto.nameALbl_i = function () {
+		var t = new eui.Label();
+		this.nameALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 10.000000000000007;
+		return t;
+	};
+	_proto.assigneeALbl_i = function () {
+		var t = new eui.Label();
+		this.assigneeALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 54;
+		return t;
+	};
+	_proto.membershipALbl_i = function () {
+		var t = new eui.Label();
+		this.membershipALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 94;
+		return t;
+	};
+	_proto.yrALbl_i = function () {
+		var t = new eui.Label();
+		this.yrALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 134;
+		return t;
+	};
+	_proto.starALbl_i = function () {
+		var t = new eui.Label();
+		this.starALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 174;
+		return t;
+	};
+	_proto.stemALbl_i = function () {
+		var t = new eui.Label();
+		this.stemALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 214;
+		return t;
+	};
+	_proto.seasonALbl_i = function () {
+		var t = new eui.Label();
+		this.seasonALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 254;
+		return t;
+	};
+	_proto.branchALbl_i = function () {
+		var t = new eui.Label();
+		this.branchALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 294;
+		return t;
+	};
+	_proto.constellationALbl_i = function () {
+		var t = new eui.Label();
+		this.constellationALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 334;
+		return t;
+	};
+	_proto.actualALbl_i = function () {
+		var t = new eui.Label();
+		this.actualALbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 374;
+		return t;
+	};
+	_proto.grpB_i = function () {
+		var t = new eui.Group();
+		this.grpB = t;
+		t.x = 605;
+		t.elementsContent = [this.nameBLbl_i(),this.assigneeBLbl_i(),this.membershipBLbl_i(),this.yrBLbl_i(),this.starBLbl_i(),this.stemBLbl_i(),this.seasonBLbl_i(),this.branchBLbl_i(),this.constellationBLbl_i(),this.actualBLbl_i()];
+		return t;
+	};
+	_proto.nameBLbl_i = function () {
+		var t = new eui.Label();
+		this.nameBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 10.000000000000007;
+		return t;
+	};
+	_proto.assigneeBLbl_i = function () {
+		var t = new eui.Label();
+		this.assigneeBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 54;
+		return t;
+	};
+	_proto.membershipBLbl_i = function () {
+		var t = new eui.Label();
+		this.membershipBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 94;
+		return t;
+	};
+	_proto.yrBLbl_i = function () {
+		var t = new eui.Label();
+		this.yrBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 134;
+		return t;
+	};
+	_proto.starBLbl_i = function () {
+		var t = new eui.Label();
+		this.starBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 174;
+		return t;
+	};
+	_proto.stemBLbl_i = function () {
+		var t = new eui.Label();
+		this.stemBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 214;
+		return t;
+	};
+	_proto.seasonBLbl_i = function () {
+		var t = new eui.Label();
+		this.seasonBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 254;
+		return t;
+	};
+	_proto.branchBLbl_i = function () {
+		var t = new eui.Label();
+		this.branchBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 294;
+		return t;
+	};
+	_proto.constellationBLbl_i = function () {
+		var t = new eui.Label();
+		this.constellationBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 334;
+		return t;
+	};
+	_proto.actualBLbl_i = function () {
+		var t = new eui.Label();
+		this.actualBLbl = t;
+		t.scaleX = 1;
+		t.scaleY = 1;
+		t.size = 28;
+		t.text = "---";
+		t.textAlign = "center";
+		t.verticalAlign = "middle";
+		t.width = 195;
+		t.y = 374;
 		return t;
 	};
 	return resultPageSkin;
